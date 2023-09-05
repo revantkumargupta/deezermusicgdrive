cf83c35f17e4820b50d7baa8da586f4e341f280f
Revant Kumar Gupta <125722896+revantkumargupta@users.noreply.github.com>
2023-09-05 16:05:07 +0545

    Update keys.py

--- keys.py
+++ keys.py
@@ -1,6 +1,11 @@
-api_id = ''
-api_hash = ''
-bot_token = ''
-arl_token = ''
-index_link = ''
-db_url = ''
+from decouple import Config, Csv
+
+config = Config()
+
+# Load the variables from the .env file
+api_id = config('api_id')
+api_hash = config('api_hash')
+bot_token = config('bot_token')
+arl_token = config('arl_token')
+index_link = config('index_link')
+db_url = config('db_url')
